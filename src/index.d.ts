acbd3a85dbe889dbf494dbee6619e70c48b7e88b
Jeffrey Barron <me@jeffreybarron.dev>
2024-10-25 20:45:08 -0700

    Adjust dependencies

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 export function createContext(ctx: any): Promise<Context>;
 
-export * from "@prisma/client/index.d.ts";
+export * from "@prisma/client";
 
 export interface Context {
     prisma: PrismaClient;
