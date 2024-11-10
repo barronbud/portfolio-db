bc7d6a1b801c418ac3d646dd10af3cab7911a659
Jeffrey Barron <me@jeffreybarron.dev>
2024-11-09 20:14:11 -0800

    Disable zod schema generator and manually change models

--- prisma/zod/oms_order.ts
+++ prisma/zod/oms_order.ts
@@ -1,20 +1,34 @@
-import * as z from "zod"
-import { Completeoms_Customer, Relatedoms_CustomerModel, Completeoms_OrderItem, Relatedoms_OrderItemModel } from "./index"
+import * as z from "zod";
+import {
+    Completeoms_Customer,
+    Relatedoms_CustomerModel,
+    Completeoms_OrderItem,
+    Relatedoms_OrderItemModel,
+} from "./index";
 
 export const oms_OrderModel = z.object({
-  id: z.string().optional(),
-  status: z.string().min(1, { message: "Status is required" }),
-  total: z.number().int().positive({ message: "Total must be greater than 0" }),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
-  customerId: z.string().min(1, { message: "Customer ID is required" }),
-  shipping: z.number().int().nonnegative({ message: "Shipping must be greater than or equal to 0" }),
-  tax: z.number().int().nonnegative({ message: "Tax must be greater than or equal to 0" }),
-})
+    id: z.string().optional(),
+    status: z.string().min(1, { message: "Status is required" }),
+    total: z
+        .number()
+        .int()
+        .positive({ message: "Total must be greater than 0" }),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+    customerId: z.string().min(1, { message: "Customer ID is required" }),
+    shipping: z.coerce
+        .number()
+        .nonnegative({
+            message: "Shipping must be greater than or equal to 0",
+        }),
+    tax: z.coerce
+        .number()
+        .nonnegative({ message: "Tax must be greater than or equal to 0" }),
+});
 
 export interface Completeoms_Order extends z.infer<typeof oms_OrderModel> {
-  customer: Completeoms_Customer
-  orderItems: Completeoms_OrderItem[]
+    customer: Completeoms_Customer;
+    orderItems: Completeoms_OrderItem[];
 }
 
 /**
@@ -22,7 +36,10 @@ export interface Completeoms_Order extends z.infer<typeof oms_OrderModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const Relatedoms_OrderModel: z.ZodSchema<Completeoms_Order> = z.lazy(() => oms_OrderModel.extend({
-  customer: Relatedoms_CustomerModel,
-  orderItems: Relatedoms_OrderItemModel.array(),
-}))
+export const Relatedoms_OrderModel: z.ZodSchema<Completeoms_Order> = z.lazy(
+    () =>
+        oms_OrderModel.extend({
+            customer: Relatedoms_CustomerModel,
+            orderItems: Relatedoms_OrderItemModel.array(),
+        })
+);
